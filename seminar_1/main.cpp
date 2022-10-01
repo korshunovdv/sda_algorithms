66f8c2a704539406ae307a366afb596bb7da136f
Dmitry Korshunov <korshunovdv@gmail.com>
2022-10-01 17:32:36 +0400

    seminar_1: add int overflow

--- seminar_1/main.cpp
+++ seminar_1/main.cpp
@@ -1,8 +1,18 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <cassert>
+#include <random>
+#include <algorithm>
+#include <cstdio>
+#include <exception>
+
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	int x = 1;
+	for (int i = 0; i < 100; ++i) {
+		x += x * 2;
+		std::cout << "index " << i << " = " << x << "\n";
+	}
 	return 0;
 
 };
